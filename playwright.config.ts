860953059af8c0d81c9b6dafdf89fdfc8d98e3e1
skbnawaz <skbnawaz3@gmail.com>
2026-06-09 11:16:18 +0530

    junit config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: "never" }]],
+  reporter: [
+    ["html", { open: "never" }],
+    ["junit", { outputFile: "junt-playwright-report.xml" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
